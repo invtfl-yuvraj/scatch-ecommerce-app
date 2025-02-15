707c0b160ca29f616ef2e74cd82299a9070e42e8
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2025-02-16 00:03:24 +0530

    Created a util to get colors

---
+++ utils/generateColor.js
@@ -0,0 +1,28 @@
+data = [
+  {
+    name: "brown",
+    textcolor: "#774F3D",
+    panelcolor: "#DEBEAE",
+    bgcolor: "#FADDD2",
+  },
+  {
+    name: "lightblue",
+    textcolor: "#48606E",
+    panelcolor: "#D3E4EE",
+    bgcolor: "#F3F8FB",
+  },
+  {
+    name: "khaakibrown",
+    textcolor: "#5F4A30",
+    panelcolor: "#C5B095",
+    bgcolor: "#DEDOBF",
+  },
+  {
+    name: "pink",
+    textcolor: "#816264",
+    panelcolor: "#E3C4C6",
+    bgcolor: "#F4E0E1",
+  },
+];
+
+module.exports = data;
